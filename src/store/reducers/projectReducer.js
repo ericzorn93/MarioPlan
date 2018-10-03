7b58da74e98ada18574129f85c5893540e29f104
Eric Zorn <ericzorn@Erics-MBP-2018.home>
2018-10-03 13:08:29 -0400

    Add Project without Rerouting

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -10,12 +10,18 @@ const projectReducer = (state = initState, action) => {
   switch (action.type) {
     case "CREATE_PROJECT":
       console.log("create project", action.project);
+      return state;
+      break;
+
+    case "CREATE_PROJECT_ERROR":
+      console.log("create project error", action.error);
+      return state;
       break;
 
     default:
+      return state;
       break;
   }
-  return state;
 };
 
 export default projectReducer;
