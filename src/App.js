59c53003a6a191acb68833482b2d07b830de5b50
Eric Zorn <ericzorn@Erics-MBP-2018.home>
2018-10-03 12:44:29 -0400

    Setting Up Firebase with Firestore and Redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
 import Dashboard from "./components/dashboard/Dashboard";
 import ProjectDetails from "./components/projects/ProjectDetails";
 import SignIn from "./components/auth/SignIn";
 import SignUp from "./components/auth/Signup";
-import CreateProject from './components/projects/CreateProject';
+import CreateProject from "./components/projects/CreateProject";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <Navbar/>
-            <Switch>
-                <Route path='/' exact component={Dashboard} />
-                <Route path='/project/:id' exact component={ProjectDetails} />
-                <Route path='/signin' exact component={SignIn} />
-                <Route path='/signup' exact component={SignUp} />
-                <Route path='/create' exact component={CreateProject} />
-            </Switch>
+          <Navbar />
+          <Switch>
+            <Route path="/" exact component={Dashboard} />
+            <Route path="/project/:id" exact component={ProjectDetails} />
+            <Route path="/signin" exact component={SignIn} />
+            <Route path="/signup" exact component={SignUp} />
+            <Route path="/create" exact component={CreateProject} />
+          </Switch>
         </div>
       </BrowserRouter>
     );
--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,11 +1,10 @@
-import authReducer from './authReducer';
-import projectReducer from './projectReducer';
-import { combineReducers } from 'redux';
-
+import authReducer from "./authReducer";
+import projectReducer from "./projectReducer";
+import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
-    auth: authReducer,
-    project: projectReducer
+  auth: authReducer,
+  project: projectReducer
 });
 
 export default rootReducer;
