dc6cd71420331aac979d77547788d1f47ff88438
Eric Zorn <ericzorn@Erics-MBP-2018.home>
2018-10-05 22:52:02 -0400

    Add Individual Projects with React Router and Firebase Redux

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import ProjectSummary from './ProjectSummary';
+import React from "react";
+import ProjectSummary from "./ProjectSummary";
+import { Link } from "react-router-dom";
 
 const ProjectList = ({ projects }) => {
-    return (
-        <div className="project-list section">
-            { projects && projects.map(project => {
-                return (
-                    <ProjectSummary project={project} key={project.id} />
-                )
-            }) }
-        </div>
-    )
+  return (
+    <div className="project-list section">
+      {projects &&
+        projects.map(project => {
+          return (
+            <Link to={`/project/${project.id}`}>
+              <ProjectSummary project={project} key={project.id} />
+            </Link>
+          );
+        })}
+    </div>
+  );
 };
 
 export default ProjectList;
