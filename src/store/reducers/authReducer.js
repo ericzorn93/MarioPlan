761d16209e3f18791a42ef99961f6073ce1f807a
Eric Zorn <ericzorn@Erics-MBP-2018.home>
2018-10-10 11:37:38 -0400

    Adding User Login with Auth Reducer and Auth Actions in Redux

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,7 +1,28 @@
-const initState = {};
+const initState = {
+  authError: null
+};
 
 const authReducer = (state = initState, action) => {
-  return state;
+  switch (action.type) {
+    case "LOGIN_ERROR":
+      console.log("login error");
+      return {
+        ...state,
+        authError: "Login Failed!"
+      };
+      break;
+
+    case "LOGIN_SUCCESS":
+      console.log("login success");
+      return {
+        ...state,
+        authError: null
+      };
+
+    default:
+      return state;
+      break;
+  }
 };
 
 export default authReducer;
