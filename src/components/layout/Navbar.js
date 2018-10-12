8453ab8c6fb3ae571dd4bb7ad282b0dea21212fd
Eric Zorn <ericzorn@Erics-MBP-2018.home>
2018-10-12 12:11:21 -0400

    Add User Authentication Dynamic Login/Logout Links in Navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,23 +5,25 @@ import SignedOutLinks from "./SignedOutLinks";
 import { connect } from "react-redux";
 
 const Navbar = props => {
+  const { auth } = props;
+  // console.log(auth);
+  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
   return (
     <nav className="anv-wrapper grey darken-3">
       <div className="container">
         <Link to="/" className="brand-logo">
           MarioPlan
         </Link>
-        <SignedInLinks />
-        <SignedOutLinks />
+        {links}
       </div>
     </nav>
   );
 };
 
 const mapStateToProps = state => {
-  console.log(state);
+  // console.log(state);
   return {
-    firebase: state.firebase
+    auth: state.firebase.auth
   };
 };
 
