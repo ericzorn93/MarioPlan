81de7df4f75ff3d2eaaff683f0c220241aabead4
Eric Zorn <ericzorn@Erics-MBP-2018.home>
2018-10-19 22:59:59 -0400

    Reformatting User and Date String

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -2,16 +2,20 @@ export const createProject = project => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     // make async call to database
     const firestore = getFirestore();
+    const profile = getState().firebase.profile;
+    const authorId = getState().firebase.auth.uid;
+
     firestore
       .collection("projects")
       .add({
         ...project,
-        authorFirstName: "Net",
-        authorLastName: "Ninja",
-        authorId: 12345,
+        authorFirstName: profile.firstName || "Random User",
+        authorLastName: profile.lastName || "Random Message",
+        authorId: authorId || Math.floor(Math.random() * 99999999),
         createdAt: new Date()
       })
       .then(() => {
+        console.log(firestore);
         dispatch({ type: "CREATE_PROJECT", project });
       })
       .catch(error => {
